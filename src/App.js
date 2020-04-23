946e2fc615ef537071a0efac009b0032a950223a
basiclines <igonzaleznicolas@gmail.com>
2020-04-23 11:53:59 +0200

    Added initial plugin scaffold

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'src/ui/Bulletproof.css'
+import 'src/App.css'
+import 'src/ui/FigmaUI.css'
+
+import Element from 'src/ui/Element'
+
+class ui extends Element {
+
+	render() {
+		return`
+			
+		`
+	}
+}
+
+customElements.define('root-ui', ui)
